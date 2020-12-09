31cf7ec28f304fdfb69dd5245030c50f36805135
Ubuntu <arunkalyanakuma@arunkalyanakuma.oxpt2r3mz4puboqlm5n1biww0f.gx.internal.cloudapp.net>
2020-12-09 23:35:45 +0000

    first commit for java project

---
+++ src/main/java/com/Pegasus95/jenkins_docker_pipeline/App.java
@@ -0,0 +1,13 @@
+package com.Pegasus95.jenkins_docker_pipeline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/Pegasus95/jenkins_docker_pipeline/AppTest.java
@@ -0,0 +1,38 @@
+package com.Pegasus95.jenkins_docker_pipeline;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
