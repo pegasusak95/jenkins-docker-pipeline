6be4fcb21ebeab7a63dd7d47c3d148238962f0f3
Ubuntu <arunkalyanakuma@arunkalyanakuma.oxpt2r3mz4puboqlm5n1biww0f.gx.internal.cloudapp.net>
2020-12-10 00:45:50 +0000

    updated project with new class file

---
+++ src/main/java/com/Pegasus95/jenkins_docker_pipeline/dockertest.java
@@ -0,0 +1,10 @@
+package com.Pegasus95.jenkins_docker_pipeline;
+
+public class dockertest {
+	
+	public static void main(String[] Args) {
+		String text="This is asample text to update project";
+		System.out.println(text);
+	}
+
+}
